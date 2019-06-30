4a5034841925e25ec70de4d42a925308d89593b8
Antonin RAFFIN <antonin.raffin@ensta.org>
2019-06-30 12:51:41 +0200

    Add tests for TD3

--- tests/test_continuous.py
+++ tests/test_continuous.py
@@ -5,7 +5,7 @@ import gym
 import pytest
 import numpy as np
 
-from stable_baselines import A2C, SAC, DDPG, PPO1, PPO2, TRPO
+from stable_baselines import A2C, SAC, DDPG, PPO1, PPO2, TRPO, TD3
 # TODO: add support for continuous actions
 # from stable_baselines.acer import ACER
 # from stable_baselines.acktr import ACKTR
@@ -27,6 +27,7 @@ MODEL_LIST = [
     PPO1,
     PPO2,
     SAC,
+    TD3,
     TRPO
 ]
 
@@ -83,7 +84,7 @@ def test_model_manipulation(request, model_class):
         with pytest.warns(None) as record:
             act_prob = model.action_probability(obs)
 
-        if model_class in [DDPG, SAC]:
+        if model_class in [DDPG, SAC, TD3]:
             # check that only one warning was raised
             assert len(record) == 1, "No warning was raised for {}".format(model_class)
             assert act_prob is None, "Error: action_probability should be None for {}".format(model_class)
@@ -100,7 +101,7 @@ def test_model_manipulation(request, model_class):
         observations = observations.reshape((-1, 1))
         actions = np.array([env.action_space.sample() for _ in range(10)])
 
-        if model_class == DDPG:
+        if model_class in [DDPG, TD3]:
             with pytest.raises(ValueError):
                 model.action_probability(observations, actions=actions)
         else:
